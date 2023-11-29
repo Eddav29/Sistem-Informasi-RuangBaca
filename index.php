d5572e93e45abe3516018018139b4f5c4fbe4492
Muhammad Fathurrozak Al Qoroni <116018304+AlQoronii@users.noreply.github.com>
2023-11-29 21:55:42 +0700

    70% / 100% Home Dashboard

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <!-- 
+<!-- 
 if (session_status() === PHP_SESSION_NONE)
     session_start();
 if (!empty($_SESSION['level'])) {
@@ -43,13 +43,14 @@ if (!empty($_SESSION['level'])) {
 //     include "Config/koneksi.php";
 // if(!isset($_SESSION["username"]) || $_SESSION["level"] == 'Admin'){
 //     include 'App/Admin/indexAdmin.php';
-// }
+// 
 
 
-if (session_status() === PHP_SESSION_NONE)
+if (session_status() === PHP_SESSION_NONE){
     session_start();
+}
     include "Config/koneksi.php";
-if(isset($_SESSION["username"]) || $_SESSION["level"] == 'Admin'){
+if(isset($_SESSION["username"]) && $_SESSION["userLevel"] == "Admin"){
     if(!empty($_GET['page'])){
         include 'App/Admin/header.php';
         include 'App/Admin/' . $_GET['page'] . '/index.php';
@@ -58,3 +59,33 @@ if(isset($_SESSION["username"]) || $_SESSION["level"] == 'Admin'){
         include 'App/Admin/indexAdmin.php';
     }
 }
+
+
+// <?php
+// if (session_status() === PHP_SESSION_NONE) {
+//     session_start();
+// }
+
+// include "Config/koneksi.php";
+
+// if (isset($_SESSION["username"]) && isset($_SESSION["userLevel"])) {
+//     // Check if the user level is 'Admin'
+//     if ($_SESSION["userLevel"] == 'Admin') {
+//         if (!empty($_GET['page'])) {
+//             include 'App/Admin/header.php';
+//             include 'App/Admin/' . $_GET['page'] . '/index.php';
+//             include 'App/Admin/footer.php';
+//         } else {
+//             include 'App/Admin/indexAdmin.php';
+//         }
+//     } else {
+//         // Handle other user levels (if needed)
+//         header("Location: non_admin_page.php");
+//         exit();
+//     }
+// } else {
+//     // Redirect to login page if not logged in
+//     header("Location: login.php");
+//     exit();
+// }
+// ?>
