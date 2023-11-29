868c3cc9814da97b7e6f9e4f6e2f5c490222767f
YunilaPutmasari <nilaputma26@gmail.com>
2023-11-29 17:36:50 +0700

    membuat crud indexTransaksi.php

--- Config/koneksi.php
+++ Config/koneksi.php
@@ -1,8 +1,23 @@
 <?php
-date_default_timezone_set("Asia/Jakarta");
-$koneksi = mysqli_connect("localhost", "root", "", "ruangBaca");
+class Database
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "db_ruangbaca";
+    private $conn;
 
-if (mysqli_connect_errno()){
-    die("Koneksi database gagal: " . mysqli_connect_error());
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
 }
-?>
