160112ccb08f826c78d8201df94f6b4dfb92882e
Muhammad Fathurrozak Al Qoroni <116018304+AlQoronii@users.noreply.github.com>
2023-12-06 13:09:48 +0700

    Member + Buku CRUD

--- Functions/Book.php
+++ Functions/Book.php
@@ -3,6 +3,7 @@
 class Book
 {
     private $conn;
+    
 
     public function __construct($conn)
     {
@@ -145,7 +146,98 @@ public function addBookFromForm()
 }
 
 
+public function edit($judul_buku, $deskripsi, $ketersediaan, $tanggal_pengadaan, $tahun_penerbit, $penerbit, $rak, $img, $status_buku, $id){
+    $judul_buku = mysqli_real_escape_string($this->conn, $judul);
+    $deskripsi = mysqli_real_escape_string($this->conn, $deskripsi);
+    $ketersediaan = mysqli_real_escape_string($this->conn, $ketersediaan);
+    $tanggal_pengadaan = mysqli_real_escape_string($this->conn, $tanggal_pengadaan);
+    $tahun_terbit = mysqli_real_escape_string($this->conn, $tahun_terbit);
+    $penerbit = mysqli_real_escape_string($this->conn, $penerbit);
+    $rak = mysqli_real_escape_string($this->conn, $rak);
+    $img = mysqli_real_escape_string($this->conn, $img);
+    $status_buku = mysqli_real_escape_string($this->conn, $status_buku);
+    $id = mysqli_real_escape_string($this->conn, $id);
+
+    $queryEditBuku = "UPDATE buku SET 
+    JUDUL_BUKU = '$judul_buku',
+    DESKRIPSI = '$deskripsi',
+    KETERSEDIAAN = '$ketersediaan',
+    TANGGAL_PENGADAAN = '$tanggal_pengadaan',
+    TAHUN_TERBIT = '$tahun_terbit',
+    PENERBIT = '$penerbit',
+    RAK = '$rak',
+    IMG = '$img',
+    STATUS_BUKU = '$status_buku'
+    WHERE ID_BUKU = $id";
+
+    $result = mysqli_query($this->conn, $queryEditBuku);
+
+    return $result;
+    
+}
+
+public function editBookFromForm()
+{
+    if (isset($_POST['save'])) {
+
+        $id = $_POST['bookId'];  // Remove the # symbol
+        $judul_buku = $_POST['judul_buku1'];
+        $deskripsi = $_POST['deskripsi1'];
+        $ketersediaan = $_POST['ketersediaan1'];
+        $tanggal_pengadaan = $_POST['tanggal_pengadaan1'];
+        $tahun_terbit = $_POST['tahun_terbit1'];
+        $penerbit = $_POST['penerbit1'];
+        $rak = $_POST['rak1'];
+        $img = $_POST['img1'];
+        $status_buku = $_POST['status_buku1'];
+
+        
+
+        // Assuming $book is an instance of your Book class
+        $result = $this->edit($judul_buku, $deskripsi, $ketersediaan, $tanggal_pengadaan, $tahun_penerbit, $penerbit, $rak, $img, $status_buku, $id);
+
+        if ($result) {
+            ob_start();
+
+            pesan('success', "Buku Telah Diupdate.");
+            header("Location: index.php?page=Buku");
+        } else {
+            pesan('danger', "Gagal Edit Buku Karena: " . mysqli_error($this->conn));
+        }
+    } else {
+        echo "err";
+    }
+}
 
 
+
+public function hapusBuku($idBuku)
+    {
+        $idBuku = mysqli_real_escape_string($this->conn, $idBuku); // Escape ID
+
+
+        $delete_query = "DELETE FROM buku WHERE ID_BUKU = '$idBuku'"; // 
+
+        $result = mysqli_query($this->conn, $delete_query);
+
+        return $result;
+    }
+    public function hapusBukuFromForm()
+    {
+        if (isset($_GET['idBuku'])) {
+            $idBuku = $_GET['idBuku'];
+
+            $result = $this->hapusBuku($idBuku);
+
+            if ($result) {
+                pesan('success', 'Buku Telah Dihapus.');
+            } else {
+                pesan('danger', 'Gagal Menghapus Buku: ' . mysqli_error($this->conn));
+            }
+
+            header("Location: index.php?page=buku");
+            exit;
+        }
+    }
 
 }
--- Login/cek_login.php
+++ Login/cek_login.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     exit();
                 } elseif ($userLevel === 'Member') {
-                    header("Location: ../App/member/index.php");
+                    header("Location: ../index.php?");
                     exit();
                 }
             } else {
