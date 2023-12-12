a2e5b839fbc40b11b14a21876ad3fe5951a35ba9
syahrulfrh <syahrulfrh@gmail.com>
2023-12-12 23:07:29 +0700

    ngefix bug

--- App/Member/footerMember.php
+++ App/Member/footerMember.php
@@ -1,7 +1,7 @@
-<script src="Assets2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="Assets/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
-<script src="Assets2/custom/dashboard.js"></script>
+<script src="Assets/js/dashboard.js"></script>
 </div>
 </div>
 </div>
@@ -42,6 +42,12 @@
         });
     });
 </Script>
+<script src="Assets/DataTables/datatables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.table-data').DataTable();
+    });
+</script>
 </body>
 
 </html>
